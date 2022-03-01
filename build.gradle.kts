3e191c32d7ad5a15f223160d749d31f1a7daea0b
Paul Griffith <pgriffith@ia.io>
2022-02-28 16:03:00 -0800

    JDeploy experimentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-import org.gradle.internal.os.OperatingSystem
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.util.capitalizeDecapitalize.capitalizeFirstWord
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE
 
 @Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
@@ -9,12 +7,11 @@ plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.serialization)
     alias(libs.plugins.ktlint)
-    alias(libs.plugins.runtime)
-    alias(libs.plugins.sonarqube)
+    alias(libs.plugins.shadow)
 }
 
 group = "io.github.paulgriffith"
-version = "0.0.6"
+//version = "0.0.6"
 
 repositories {
     mavenCentral()
@@ -40,21 +37,17 @@ dependencies {
     implementation(libs.swingx)
     implementation(libs.logback)
     implementation(libs.svgSalamander)
+    implementation(libs.osthemedetector)
     implementation(libs.bundles.coroutines)
     implementation(libs.bundles.flatlaf)
     implementation(libs.bundles.ignition) {
         // Exclude transitive IA dependencies - we only need core Ignition classes for cache deserialization
         isTransitive = false
     }
-    implementation(libs.osthemedetector)
     runtimeOnly(libs.bundles.ia.transitive)
 
     testImplementation(libs.bundles.kotest)
 }
-
-// add-exports is used to bypass Java modular restrictions
-val defaultJvmArgs = listOf("--add-exports", "java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED")
-val className = "io.github.paulgriffith.MainPanel"
 
 tasks {
     test {
@@ -70,82 +63,19 @@ tasks {
     }
     jar {
         manifest {
-            attributes["Main-Class"] = className
+            attributes["Main-Class"] = "io.github.paulgriffith.MainPanel"
         }
     }
-}
-
-application {
-    mainClass.set(className)
-    applicationName = rootProject.name.capitalizeFirstWord()
-    applicationDefaultJvmArgs = defaultJvmArgs
+    shadowJar {
+        archiveBaseName.set("kindling")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+    }
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get()))
-        vendor.set(JvmVendorSpec.AZUL)
-    }
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "paul-griffith_kindling")
-        property("sonar.organization", "paul-griffith")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
-runtime {
-    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-
-    modules.set(
-        listOf(
-            "java.desktop",
-            "java.sql",
-            "java.logging",
-            "java.naming",
-            "java.xml",
-        )
-    )
-
-    jpackage {
-        val currentOs = OperatingSystem.current()
-        val imgType = when {
-            currentOs.isWindows -> "ico"
-            currentOs.isMacOsX -> "icns"
-            else -> "png"
-        }
-        // Reverse the package version because MacOS doesn't like leading zeroes
-        appVersion = if (currentOs.isMacOsX) version.toString().reversed() else version.toString()
-        imageOptions = listOf("--icon", "src/main/resources/icons/ignition.$imgType")
-        @OptIn(ExperimentalStdlibApi::class)
-        val options: Map<String, String?> = buildMap {
-            put("resource-dir", "src/main/resources")
-            put("vendor", "Paul Griffith")
-            put("copyright", "2022")
-            put("description", "A collection of useful tools for troubleshooting Ignition")
-
-            when {
-                currentOs.isWindows -> {
-                    put("win-per-user-install", null)
-                    put("win-dir-chooser", null)
-                    put("win-menu", null)
-                    put("win-shortcut", null)
-                    // random (consistent) UUID makes upgrades smoother
-                    put("win-upgrade-uuid", "8e7428c8-bbc6-460a-9995-db6d8b04a690")
-                }
-                currentOs.isLinux -> {
-                    put("linux-shortcut", null)
-                }
-            }
-        }
-
-        jvmArgs = defaultJvmArgs
-
-        installerOptions = options.flatMap { (key, value) ->
-            listOfNotNull("--$key", value)
-        }
     }
 }
 
