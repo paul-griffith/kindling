c2fe3822d55c4158a8c81144e307fae9548158f9
Paul Griffith <pgriffith@ia.io>
2022-08-10 16:35:16 -0700

    Reintroduce JPackage, experiment with GH actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.internal.os.OperatingSystem
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE
 
@@ -9,6 +10,7 @@ plugins {
     alias(libs.plugins.serialization)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.shadow)
+    alias(libs.plugins.runtime)
 }
 
 group = "io.github.paulgriffith"
@@ -86,8 +88,64 @@ java {
 }
 
 ktlint {
-    disabledRules.add("trailing-comma-on-call-site")
     reporters {
         reporter(CHECKSTYLE)
+    }
+}
+
+runtime {
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+
+    modules.set(
+        listOf(
+            "java.desktop",
+            "java.sql",
+            "java.logging",
+            "java.naming",
+            "java.xml"
+        )
+    )
+
+    jpackage {
+        val currentOs = OperatingSystem.current()
+        val imgType = when {
+            currentOs.isWindows -> "ico"
+            currentOs.isMacOsX -> "icns"
+            else -> "png"
+        }
+        // Reverse the package version because MacOS doesn't like leading zeroes
+        appVersion = System.getenv("TAG_VERSION") ?: "1.0.0"
+        imageOptions = listOf("--icon", "src/main/resources/icons/ignition.$imgType")
+        @OptIn(ExperimentalStdlibApi::class)
+        val options: Map<String, String?> = buildMap {
+            put("resource-dir", "src/main/resources")
+            put("vendor", "Paul Griffith")
+            put("copyright", "2022")
+            put("description", "A collection of useful tools for troubleshooting Ignition")
+
+            when {
+                currentOs.isWindows -> {
+                    put("win-per-user-install", null)
+                    put("win-dir-chooser", null)
+                    put("win-menu", null)
+                    put("win-shortcut", null)
+                    // random (consistent) UUID makes upgrades smoother
+                    put("win-upgrade-uuid", "8e7428c8-bbc6-460a-9995-db6d8b04a690")
+                }
+
+                currentOs.isLinux -> {
+                    put("linux-shortcut", null)
+                }
+            }
+        }
+
+        // add-exports is used to bypass Java modular restrictions
+        jvmArgs = listOf("--add-exports", "java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED")
+
+        installerOptions = options.flatMap { (key, value) ->
+            listOfNotNull("--$key", value)
+        }
+
+        mainJar = "kindling-bundle.jar"
     }
 }
