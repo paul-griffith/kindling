70fac29f83cdda9e7a6165457526fb37623904a0
joshlha <joshlha16@gmail.com>
2022-11-02 08:26:42 -0700

    More work on MetricTree

--- idb/src/main/kotlin/io/github/paulgriffith/kindling/idb/metrics/MetricTree.kt
+++ idb/src/main/kotlin/io/github/paulgriffith/kindling/idb/metrics/MetricTree.kt
@@ -6,14 +6,35 @@ import io.github.paulgriffith.kindling.utils.PathNode
 import io.github.paulgriffith.kindling.utils.TypedTreeNode
 import io.github.paulgriffith.kindling.utils.treeCellRenderer
 import javax.swing.JTree
+import javax.swing.tree.DefaultTreeModel
 
-class MetricNode(override val userObject: Metric) : TypedTreeNode<Metric>()
+class MetricNode(override val userObject: String) : TypedTreeNode<String>()
 
-class MetricTree(metrics: List<Metric>) : CheckBoxTree() {
+class MetricModel(metrics: List<Metric>) : DefaultTreeModel(RootNode(metrics))
+
+class RootNode(metrics: List<Metric>) : AbstractTreeNode() {
+    init {
+        metrics.forEach { metric ->
+            val currentNode = this
+            val fullPath: String = metric.getFullTreePath()
+
+            fullPath.split(".").forEach { part ->
+                val next: MetricNode? = null
+                val children = currentNode.children.asSequence()
+            }
+        }
+    }
+}
+
+class MetricTree(metrics: List<Metric>) : CheckBoxTree(MetricModel(metrics)) {
 
     init {
         isRootVisible = false
         setShowsRootHandles(true)
     }
 
+}
+
+fun Metric.getFullTreePath(): String {
+    return this.isLegacy.toString() + "." + this.name
 }
