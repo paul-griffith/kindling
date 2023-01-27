046e00fa0493fdc91d0d4f76d44a8ad4a2cb79c5
Paul Griffith <pgriffith@ia.io>
2023-01-26 19:42:40 -0800

    Better API, currently not working

--- src/main/kotlin/io/github/inductiveautomation/kindling/thread/model/Thread.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/thread/model/Thread.kt
@@ -4,6 +4,7 @@ import io.github.inductiveautomation.kindling.utils.StackTrace
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.lang.Thread.State
+import java.lang.management.ThreadInfo
 
 @Serializable
 data class Thread(
@@ -22,6 +23,28 @@ data class Thread(
     val blocker: Blocker? = null,
     val stacktrace: StackTrace = emptyList(),
 ) {
+    constructor(data: ThreadInfo) : this(
+        id = data.threadId.toInt(),
+        name = data.threadName,
+        state = data.threadState,
+        isDaemon = data.isDaemon,
+//        cpuUsage = null,
+//        lockedMonitors = data.lockedMonitors.map {
+//            Monitors(
+//                lock = it.toString(),
+//                frame = it.lockedStackFrame?.toString(),
+//            )
+//        },
+//        lockedSynchronizers = data.lockedSynchronizers.map(LockInfo::toString),
+        stacktrace = data.stackTrace.map(StackTraceElement::toString),
+        blocker = data.lockInfo?.let { lockInfo ->
+            Blocker(
+                lock = lockInfo.toString(),
+                owner = data.lockOwnerId.takeIf { it != -1L }?.toInt(),
+            )
+        },
+    )
+
     var marked: Boolean = false
 
     val pool: String? = extractPool(name)
