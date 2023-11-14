8dadd24e37ed87c7ded1c785b0cd80b14f894606
Joshua Hansen <joshlha16@gmail.com>
2023-11-14 15:39:29 -0800

    Finish tag export parsing logic.

---
+++ src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/model/Node.kt
@@ -0,0 +1,62 @@
+package io.github.inductiveautomation.kindling.idb.tagconfig.model
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+@Serializable(with = NodeDelegateSerializer::class)
+data class Node(
+    val id: String,
+    val providerId: Int,
+    val folderId: String?,
+    @Serializable(with = TagConfigSerializer::class)
+    val config: TagConfig,
+    val rank: Int,
+    val name: String?,
+    var resolved: Boolean = false,
+) {
+    companion object {
+        fun typesNode(providerId: Int): Node = Node(
+            id = "_types_",
+            providerId = providerId,
+            folderId = null,
+            config = TagConfig(
+                name = "_types_",
+                tagType = "Folder",
+                tags = mutableListOf(),
+            ),
+            rank = 1,
+            name = "_types_",
+        )
+
+        fun providerNode(providerId: Int, typesNode: Node? = null): Node = Node(
+            id = "tag-provider",
+            providerId = providerId,
+            folderId = null,
+            config = TagConfig(
+                name = "",
+                tagType = "Provider",
+                tags = typesNode?.let { mutableListOf(it) } ?: mutableListOf(),
+            ),
+            rank = 0,
+            name = null,
+        )
+    }
+}
+
+object NodeDelegateSerializer : KSerializer<Node> {
+    @OptIn(ExperimentalSerializationApi::class)
+    override val descriptor: SerialDescriptor = SerialDescriptor("ExportNode", TagConfig.serializer().descriptor)
+    override fun deserialize(decoder: Decoder): Node {
+        throw UnsupportedOperationException("Deserialization not supported.")
+    }
+
+    override fun serialize(encoder: Encoder, value: Node) {
+        encoder.encodeSerializableValue(TagConfigSerializer, value.config)
+    }
+}
+
+typealias NodeGroup = MutableList<Node>
--- src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/model/TagConfig.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/model/TagConfig.kt
@@ -14,89 +14,90 @@ import kotlinx.serialization.json.jsonObject
 @Serializable
 data class TagConfig(
     // Basic Properties:
-    val name: String?,
-    val tagGroup: String?,
-    val enabled: Boolean?,
+    val name: String? = null,
+    val tagGroup: String? = null,
+    val enabled: Boolean? = null,
 
     // Value Properties:
-    val tagType: String?, // Unlisted
-    val typeId: String?, // Unlisted
-    val valueSource: String?,
-    val dataType: String?,
-    val value: JsonPrimitive?,
-    val opcServer: String?, // OPC
-    val opcItemPath: JsonElement?, // OPC
-    val sourceTagPath: String?, // Derived, Reference
-    val executionMode: String?,
-    val executionRate: Int?,
-    val expression: String?, // Expression
+    val tagType: String? = null, // Unlisted
+    val typeId: String? = null, // Unlisted
+    val valueSource: String? = null,
+    val dataType: String? = null,
+    val value: JsonPrimitive? = null,
+    val opcServer: String? = null, // OPC
+    val opcItemPath: JsonElement? = null, // OPC
+    val sourceTagPath: String? = null, // Derived, Reference
+    val executionMode: String? = null,
+    val executionRate: Int? = null,
+    val expression: String? = null, // Expression
     @SerialName("deriveExpressionGetter")
-    val readExpression: String?, // Derived
+    val readExpression: String? = null, // Derived
     @SerialName("deriveExpressionSetter")
-    val writeExpression: String?, // Derived
-    val query: String?, // Query
-    val queryType: String?, // Query
-    val datasource: String?, // Query
+    val writeExpression: String? = null, // Derived
+    val query: String? = null, // Query
+    val queryType: String? = null, // Query
+    val datasource: String? = null, // Query
 
     // Numeric Properties:
-    val deadband: Double?,
-    val deadbandMode: String?,
-    val scaleMode: String?,
-    val rawLow: Double?,
-    val rawHigh: Double?,
-    val scaledLow: Double?,
-    val scaledHigh: Double?,
-    val clampMode: String?,
-    val scaleFactor: Double?,
-    val engUnit: String?,
-    val engLow: Double?,
-    val engHigh: Double?,
-    val engLimitMode: String?,
-    val formatString: String?,
+    val deadband: Double? = null,
+    val deadbandMode: String? = null,
+    val scaleMode: String? = null,
+    val rawLow: Double? = null,
+    val rawHigh: Double? = null,
+    val scaledLow: Double? = null,
+    val scaledHigh: Double? = null,
+    val clampMode: String? = null,
+    val scaleFactor: Double? = null,
+    val engUnit: String? = null,
+    val engLow: Double? = null,
+    val engHigh: Double? = null,
+    val engLimitMode: String? = null,
+    val formatString: String? = null,
 
     // Metadata Properties:
-    val tooltip: String?,
-    val documentation: String?,
-    val typeColor: JsonPrimitive?, // UDT Definitions
+    val tooltip: String? = null,
+    val documentation: String? = null,
+    val typeColor: JsonPrimitive? = null, // UDT Definitions
 
     // Security Properties
-    val readPermissions: JsonObject?,
-    val readOnly: Boolean?,
-    val writePermissions: JsonObject?,
+    val readPermissions: JsonObject? = null,
+    val readOnly: Boolean? = null,
+    val writePermissions: JsonObject? = null,
 
     // Scripting Properties
-    val eventScripts: JsonArray?,
+    val eventScripts: JsonArray? = null,
 
     // Alarm Properties
-    val alarms: JsonArray?,
-    val alarmEvalEnabled: Boolean?,
+    val alarms: JsonArray? = null,
+    val alarmEvalEnabled: Boolean? = null,
 
     // Historical Properties
-    val historyEnabled: Boolean?,
-    val historyProvider: String?,
-    val historicalDeadbandStyle: String?,
-    val historicalDeadbandMode: String?,
-    val historicalDeadband: Double?,
-    val sampleMode: String?,
-    val historySampleRate: Int?,
-    val historySampleRateUnits: String?,
-    val historyTagGroup: String?,
-    val historyTimeDeadband: Int?,
-    val historyTimeDeadbandUnits: String?,
-    val historyMaxAge: Int?,
-    val historyMaxAgeUnits: String?,
+    val historyEnabled: Boolean? = null,
+    val historyProvider: String? = null,
+    val historicalDeadbandStyle: String? = null,
+    val historicalDeadbandMode: String? = null,
+    val historicalDeadband: Double? = null,
+    val sampleMode: String? = null,
+    val historySampleRate: Int? = null,
+    val historySampleRateUnits: String? = null,
+    val historyTagGroup: String? = null,
+    val historyTimeDeadband: Int? = null,
+    val historyTimeDeadbandUnits: String? = null,
+    val historyMaxAge: Int? = null,
+    val historyMaxAgeUnits: String? = null,
+
+    val tags: NodeGroup = mutableListOf(),
 
     // UDT
-    val parameters: JsonObject?,
+    val parameters: JsonObject? = null,
 
-    // Custom Properties:
-    val customProperties: JsonObject?,
+    // Custom Properties:,
+    val customProperties: JsonObject? = null,
 )
 
 object TagConfigSerializer : JsonTransformingSerializer<TagConfig>(TagConfig.serializer()) {
     @OptIn(ExperimentalSerializationApi::class)
     override fun transformDeserialize(element: JsonElement): JsonElement {
-//        println("transformDeserialize running now...")
         val elementNames = List(TagConfig.serializer().descriptor.elementsCount) {
             TagConfig.serializer().descriptor.getElementName(it)
         }.filter { it != "customProperties" }
@@ -104,173 +105,24 @@ object TagConfigSerializer : JsonTransformingSerializer<TagConfig>(TagConfig.ser
         val customPropertiesMap = elementMap.filter { it.key !in elementNames }.onEach { (key, value) ->
             elementMap.remove(key, value)
         }
-        elementMap["customProperties"] = if (customPropertiesMap.isEmpty()) JsonNull else JsonObject(customPropertiesMap)
+        elementMap["customProperties"] =
+            if (customPropertiesMap.isEmpty()) JsonNull else JsonObject(customPropertiesMap)
         return JsonObject(elementMap)
     }
 
     override fun transformSerialize(element: JsonElement): JsonElement {
         val tagConfig = element.jsonObject.toMutableMap()
 
-        val customProperties = tagConfig.remove("customProperties")?.jsonObject?.toMutableMap() ?: return JsonObject(tagConfig)
-        customProperties.entries.forEach { (key, value) ->
+        val customProperties = tagConfig.remove("customProperties")?.let {
+            if (it is JsonNull) {
+                return JsonObject(tagConfig)
+            } else {
+                it.jsonObject
+            }
+        }
+        customProperties?.entries?.forEach { (key, value) ->
             tagConfig[key] = value
         }
         return JsonObject(tagConfig)
     }
 }
-
-// object TagConfigSerializer : KSerializer<TagConfig> {
-//    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("TagConfig") {
-//        element<String?>("name")
-//        element<String?>("tagGroup")
-//        element<Boolean?>("enabled")
-//        element<String>("tagType")
-//        element<String?>("typeId")
-//        element<String?>("valueSource")
-//        element<String?>("dataType")
-//        element<JsonPrimitive?>("value")
-//        element<String?>("opcServer")
-//        element<JsonElement?>("opcItemPath")
-//        element<String?>("sourceTagPath")
-//        element<String?>("executionMode")
-//        element<Int?>("executionRate")
-//        element<String?>("expression")
-//        element<String?>("deriveExpressionGetter")
-//        element<String?>("deriveExpressionSetter")
-//        element<String?>("query")
-//        element<String?>("queryType")
-//        element<String?>("datasource")
-//        element<Double?>("deadband")
-//        element<String?>("deadbandMode")
-//        element<String?>("scaleMode")
-//        element<Double?>("rawLow")
-//        element<Double?>("rawHigh")
-//        element<Double?>("scaledLow")
-//        element<Double?>("scaledHigh")
-//        element<String?>("clampMode")
-//        element<Double?>("scaleFactor")
-//        element<String?>("engUnit")
-//        element<Double?>("engLow")
-//        element<Double?>("engHigh")
-//        element<String?>("engLimitMode")
-//        element<String?>("formatString")
-//        element<String?>("tooltip")
-//        element<String?>("documentation")
-//        element<JsonPrimitive?>("typeColor")
-//        element<JsonObject?>("readPermissions")
-//        element<Boolean?>("readOnly")
-//        element<JsonObject?>("writePermissions")
-//        element<JsonArray?>("eventScripts")
-//        element<JsonArray?>("alarms")
-//        element<Boolean?>("alarmEvalEnabled")
-//        element<Boolean?>("historyEnabled")
-//        element<String?>("historyProvider")
-//        element<String?>("historicalDeadbandStyle")
-//        element<String?>("historicalDeadbandMode")
-//        element<Double?>("historicalDeadband")
-//        element<String?>("sampleMode")
-//        element<Int?>("historySampleRate")
-//        element<String?>("historySampleRateUnits")
-//        element<String?>("historyTagGroup")
-//        element<Int?>("historyTimeDeadband")
-//        element<String?>("historyTimeDeadbandUnits")
-//        element<Int?>("historyMaxAge")
-//        element<String?>("historyMaxAgeUnits")
-//        element<JsonObject?>("parameters")
-//        element<JsonArray?>("customProperties")
-//    }
-//
-//    override fun deserialize(decoder: Decoder): TagConfig {
-//        // Cast to JSON-specific interface
-//        val jsonInput = decoder as? JsonDecoder ?: error("Can be deserialized only by JSON")
-//
-//        // Read the whole content as JSON
-//        val json = jsonInput.decodeJsonElement().jsonObject.toMutableMap()
-//
-//        // Extract and remove name property
-//
-//        return TagConfig(
-//            name = json["name"]?.jsonPrimitive?.content.also { json.remove("name") },
-//            tagGroup = json["tagGroup"]?.jsonPrimitive?.content.also { json.remove("tagGroup") },
-//            enabled = json["enabled"]?.jsonPrimitive?.content?.toBoolean().also { json.remove("enabled") },
-//            tagType = json["tagType"]?.jsonPrimitive?.content.also { json.remove("tagType") } ?: "NoType",
-//            typeId = json["typeId"]?.jsonPrimitive?.content.also { json.remove("typeId") },
-//            valueSource = json["valueSource"]?.jsonPrimitive?.content.also { json.remove("valueSource") },
-//            dataType = json["dataType"]?.jsonPrimitive?.content.also { json.remove("dataType") },
-//            value = json["value"]?.jsonPrimitive.also { json.remove("value") },
-//            opcServer = json["opcServer"]?.jsonPrimitive?.content.also { json.remove("opcServer") },
-//            opcItemPath = json["opcItemPath"].also { json.remove("opcItemPath") },
-//            sourceTagPath = json["sourceTagPath"]?.jsonPrimitive?.content.also { json.remove("sourceTagPath") },
-//            executionMode = json["executionMode"]?.jsonPrimitive?.content.also { json.remove("executionMode") },
-//            executionRate = json["executionRate"]?.jsonPrimitive?.content?.toInt()
-//                .also { json.remove("executionRate") },
-//            expression = json["expression"]?.jsonPrimitive?.content.also { json.remove("expression") },
-//            readExpression = json["deriveExpressionGetter"]?.jsonPrimitive?.content.also { json.remove("deriveExpressionGetter") },
-//            writeExpression = json["deriveExpressionSetter"]?.jsonPrimitive?.content.also { json.remove("deriveExpressionSetter") },
-//            query = json["query"]?.jsonPrimitive?.content.also { json.remove("query") },
-//            queryType = json["queryType"]?.jsonPrimitive?.content.also { json.remove("queryType") },
-//            datasource = json["datasource"]?.jsonPrimitive?.content.also { json.remove("datasource") },
-//            deadband = json["deadband"]?.jsonPrimitive?.content?.toDouble().also { json.remove("deadband") },
-//            deadbandMode = json["deadbandMode"]?.jsonPrimitive?.content.also { json.remove("deadbandMode") },
-//            scaleMode = json["scaleMode"]?.jsonPrimitive?.content.also { json.remove("scaleMode") },
-//            rawLow = json["rawLow"]?.jsonPrimitive?.content?.toDouble().also { json.remove("rawLow") },
-//            rawHigh = json["rawHigh"]?.jsonPrimitive?.content?.toDouble().also { json.remove("rawHigh") },
-//            scaledLow = json["scaledLow"]?.jsonPrimitive?.content?.toDouble().also { json.remove("scaledLow") },
-//            scaledHigh = json["scaledHigh"]?.jsonPrimitive?.content?.toDouble().also { json.remove("scaledHigh") },
-//            clampMode = json["clampMode"]?.jsonPrimitive?.content.also { json.remove("clampMode") },
-//            scaleFactor = json["scaleFactor"]?.jsonPrimitive?.content?.toDouble().also { json.remove("scaleFactor") },
-//            engUnit = json["engUnit"]?.jsonPrimitive?.content.also { json.remove("engUnit") },
-//            engLow = json["engLow"]?.jsonPrimitive?.content?.toDouble().also { json.remove("engLow") },
-//            engHigh = json["engHigh"]?.jsonPrimitive?.content?.toDouble().also { json.remove("engHigh") },
-//            engLimitMode = json["engLimitMode"]?.jsonPrimitive?.content.also { json.remove("engLimitMode") },
-//            formatString = json["formatString"]?.jsonPrimitive?.content.also { json.remove("formatString") },
-//            tooltip = json["tooltip"]?.jsonPrimitive?.content.also { json.remove("tooltip") },
-//            documentation = json["documentation"]?.jsonPrimitive?.content.also { json.remove("documentation") },
-//            typeColor = json["typeColor"]?.jsonPrimitive.also { json.remove("typeColor") },
-//            readPermissions = json["readPermissions"]?.jsonObject.also { json.remove("readPermissions") },
-//            readOnly = json["readOnly"]?.jsonPrimitive?.content?.toBoolean().also { json.remove("readOnly") },
-//            writePermissions = json["writePermissions"]?.jsonObject.also { json.remove("writePermissions") },
-//            eventScripts = json["eventScripts"]?.jsonArray.also { json.remove("eventScripts") },
-//            alarms = json["alarms"]?.jsonArray.also { json.remove("alarms") },
-//            alarmEvalEnabled = json["alarmEvalEnabled"]?.jsonPrimitive?.content?.toBoolean()
-//                .also { json.remove("alarmEvalEnabled") },
-//            historyEnabled = json["historyEnabled"]?.jsonPrimitive?.content?.toBoolean()
-//                .also { json.remove("historyEnabled") },
-//            historyProvider = json["historyProvider"]?.jsonPrimitive?.content
-//                .also { json.remove("historyProvider") },
-//            historicalDeadbandStyle = json["historicalDeadbandStyle"]?.jsonPrimitive?.content
-//                .also { json.remove("historicalDeadbandStyle") },
-//            historicalDeadbandMode = json["historicalDeadbandMode"]?.jsonPrimitive?.content
-//                .also { json.remove("historicalDeadbandMode") },
-//            historicalDeadband = json["historicalDeadband"]?.jsonPrimitive?.content?.toDouble()
-//                .also { json.remove("historicalDeadband") },
-//            sampleMode = json["sampleMode"]?.jsonPrimitive?.content.also { json.remove("sampleMode") },
-//            historySampleRate = json["historySampleRate"]?.jsonPrimitive?.content?.toInt()
-//                .also { json.remove("historySampleRate") },
-//            historySampleRateUnits = json["historySampleRateUnits"]?.jsonPrimitive?.content
-//                .also { json.remove("historySampleRateUnits") },
-//            historyTagGroup = json["historyTagGroup"]?.jsonPrimitive?.content
-//                .also { json.remove("historyTagGroup") },
-//            historyTimeDeadband = json["historyTimeDeadband"]?.jsonPrimitive?.content?.toInt()
-//                .also { json.remove("historyTimeDeadband") },
-//            historyTimeDeadbandUnits = json["historyTimeDeadbandUnits"]?.jsonPrimitive?.content
-//                .also { json.remove("historyTimeDeadbandUnits") },
-//            historyMaxAge = json["historyMaxAge"]?.jsonPrimitive?.content?.toInt()
-//                .also { json.remove("historyMaxAge") },
-//            historyMaxAgeUnits = json["historyMaxAgeUnits"]?.jsonPrimitive?.content
-//                .also { json.remove("historyMaxAgeUnits") },
-//            parameters = json["parameters"]?.jsonObject.also { json.remove("parameters") },
-//            customProperties = JsonObject(json),
-//        )
-//    }
-//
-//    override fun serialize(encoder: Encoder, value: TagConfig) {
-//        value.name?.let(encoder::encodeString)
-//        value.customProperties?.let {
-//            val propertiesMap = it.toMap()
-//            propertiesMap.entries.forEach { _ ->
-//            }
-//        }
-//        error("Serialization is not supported")
-//    }
-// }
