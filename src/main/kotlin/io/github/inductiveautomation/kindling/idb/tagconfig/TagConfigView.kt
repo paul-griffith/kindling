dd630c5e461e61ef79d947f7dc8f4eaf1c6b634a
Joshua Hansen <joshlha16@gmail.com>
2023-11-14 15:39:29 -0800

    Finish tag export parsing logic.

--- src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/TagConfigView.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/TagConfigView.kt
@@ -1,21 +1,14 @@
 package io.github.inductiveautomation.kindling.idb.tagconfig
 
 import io.github.inductiveautomation.kindling.core.ToolPanel
+import io.github.inductiveautomation.kindling.idb.tagconfig.model.Node
+import io.github.inductiveautomation.kindling.idb.tagconfig.model.NodeGroup
 import io.github.inductiveautomation.kindling.idb.tagconfig.model.TagConfig
 import io.github.inductiveautomation.kindling.idb.tagconfig.model.TagConfigSerializer
 import io.github.inductiveautomation.kindling.utils.toList
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.buildJsonArray
-import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.encodeToStream
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.putJsonArray
 import java.awt.Font
 import java.sql.Connection
 import javax.swing.JScrollPane
@@ -26,221 +19,197 @@ import kotlin.io.path.outputStream
 @OptIn(ExperimentalSerializationApi::class)
 class TagConfigView(connection: Connection) : ToolPanel() {
 
-    private val errs = mutableListOf<Exception>()
+    private var selectedProvider: Int = 0
 
-    private val tagRecordData: List<TagRecord> =
-        connection.prepareStatement("SELECT * FROM TAGCONFIG").executeQuery().toList { rs ->
-            try {
-                TagRecord(
-                    id = rs.getString(1),
-                    providerId = rs.getInt(2),
-                    folderId = rs.getString(3),
-                    config = JSON.decodeFromString(TagConfigSerializer, rs.getString(4)),
-                    rank = rs.getInt(5),
-                    name = rs.getString(6),
-                )
-            } catch (e: NullPointerException) {
-                println("catching NPE...")
-                null
-            } catch (e: SerializationException) {
-                errs.add(e)
-                e.printStackTrace()
-                null
-            }
-        }.filterNotNull().filter { record -> record.providerId == 0 }
-    private val udtDefinitions = TagRecord.extractUdtDefinitions(tagRecordData)
+    private val preparedStatement =
+        connection.prepareStatement("SELECT * FROM TAGCONFIG WHERE PROVIDERID = ? ORDER BY ID")
+
+    private val rawNodeData: List<Node> =
+        preparedStatement.apply { setInt(1, selectedProvider) }.executeQuery()
+            .toList { rs ->
+                runCatching {
+                    Node(
+                        id = rs.getString(1),
+                        providerId = rs.getInt(2),
+                        folderId = rs.getString(3),
+                        config = JSON.decodeFromString(TagConfigSerializer, rs.getString(4)),
+                        rank = rs.getInt(5),
+                        name = rs.getString(6),
+                    )
+                }.getOrNull()
+            }.filterNotNull()
+
+    private val nodeGroups: Map<String, NodeGroup> = rawNodeData.groupBy { node ->
+        node.id.substring(0, 36)
+    }.mapValues { (_, nodes) ->
+        nodes.sortedBy { it.id }.toMutableList()
+    }.toList().sortedBy { (_, nodes) ->
+        nodes.first().rank
+    }.toMap()
+
+    private val udtDefinitions = rawNodeData.filter { it.isUdtDefinition() }.associateBy { it.getFullPath() }
+
     override val icon = null
 
-    // private val unknownKeyRegex = """Encountered an unknown key '(?<unknownKey>.*)' at path""".toRegex()
-
-    private val nullPropRegex = """\b\w+=null\b, """.toRegex()
-
     init {
-        println("tagRecordData size: ${tagRecordData.size}")
-        val tagConfigs = tagRecordData.map { record ->
-            record.config
-        }
-        val textToDisplay = nullPropRegex.replace(tagConfigs.joinToString("\n\n"), "")
+        exportToJson(selectedProvider)
 
-        val myLabel = JTextArea(textToDisplay).apply {
+        val fullPaths = udtDefinitions.keys.joinToString("\n")
+
+        val myLabel = JTextArea(fullPaths).apply {
             font = Font(Font.MONOSPACED, Font.PLAIN, 12)
         }
 
         add(JScrollPane(myLabel), "grow")
-        // doo eet
-        exportTagsToJson()
     }
 
-    private fun TagRecord.createJson(): JsonElement {
-        // check tag record / node type
-        // if type unknown
-        // // refer to udt definition map
-        // else if tag type known
-        // // what is tag type?
-        // ////// is Folder, udt instance, udt def?
-        // //////// Create JSON for the folder
-        // ////////// for any elements in folders tags array
-        // //////////// call createJson
+    private fun exportToJson(providerId: Int) {
+        val typesNode = Node.typesNode(providerId)
+        val providerNode = Node.providerNode(providerId, typesNode)
 
-        /* We don't know the tag type.
-        * Probably because this record represents an override to a pre-existing node's config.
-        * We need to find the tag record, in some definition somehwere, which contains info about this node.
-        */
-        val originalDefinition = getOriginalDefinition()
-        val (resolvedConfig, isTopLevel) = if (config.tagType.isNullOrEmpty()) {
-            config.copy(
-                name = originalDefinition.config.name,
-                tagType = originalDefinition.config.tagType,
-            ) to false
-        } else {
-            config to true
-        }
-
-        return when (resolvedConfig.tagType) {
-            "Folder",
-            "UdtInstance",
-            "UdtType",
-            -> { // This is a node representing a folder-like structure
-                require(resolvedConfig.name != null) { "tagType not null but name is! \nRecord: $this" }
-                buildJsonObject {
-                    JSON.encodeToJsonElement(
-                        TagConfigSerializer,
-                        resolvedConfig,
-                    ).jsonObject.entries.forEach { (key, value) ->
-                        put(key, value)
+        for ((_, nodeGroup) in nodeGroups) {
+            with(nodeGroup) {
+                if (parentNode.isUdtDefinition() || parentNode.isUdtInstance()) {
+                    if (!isResolved) {
+                        resolveInheritance()
                     }
-                    putJsonArray("tags") {
-                        val childNodes = if (isTopLevel) {
-                            getDirectChildren()
-                        } else {
-                            val overriddenChildren = getDirectChildren()
-                            val originalChildren = originalDefinition.getDirectChildren()
-                            val overriddenIDs = overriddenChildren.map {
-                                it.id.split(".").last()
-                            }
-
-                            overriddenChildren + originalChildren.filter { originalRecord ->
-                                val recordUUID = originalRecord.id.split(".").last()
-                                recordUUID !in overriddenIDs
-                            }
-                        }
-
-                        addAll(childNodes.map { record -> record.createJson() })
+                }
+                resolveHierarchy()
+                when (val folderId = parentNode.folderId) {
+                    "_types_" -> typesNode.config.tags.add(parentNode)
+                    null -> providerNode.config.tags.add(parentNode)
+                    else -> {
+                        val folderGroup =
+                            nodeGroups[folderId] ?: throw IllegalStateException("This should never happen")
+                        folderGroup.parentNode.config.tags.add(parentNode)
                     }
                 }
             }
-
-            "AtomicTag" -> { // We name it's name, tagType, so this is a leaf which represents an actual tag
-                require(resolvedConfig.name != null) { "tagType not null but name is! \nRecord: $this" }
-                JSON.encodeToJsonElement(TagConfigSerializer, resolvedConfig)
-            }
-
-            else -> {
-                throw IllegalArgumentException("Tag Type is not any predefined types!\n$resolvedConfig")
-            }
-        }
-    }
-
-    private fun exportTagsToJson() {
-        val topLevelDefinitionNodes = tagRecordData.filter {
-            it.folderId == "_types_"
         }
 
-        val typesFolderEntry = mutableMapOf(
-            "name" to JsonPrimitive("_types_"),
-            "tagType" to JsonPrimitive("Folder"),
-            "tags" to buildJsonArray {
-                addAll(topLevelDefinitionNodes.map { tagRecord -> tagRecord.createJson() })
-            },
-        )
-        // initialize root of tag export structure with processed tags array
-        val jsonExportData = JsonObject(
-            mutableMapOf(
-                "name" to JsonPrimitive(""),
-                "tagType" to JsonPrimitive("Provider"),
-                "tags" to JsonArray(listOf(JsonObject(typesFolderEntry))),
-            ),
-        )
         Path(
             System.getProperty("user.home"),
             "Desktop",
             "tag-export-dev-test.json",
         ).outputStream().use {
-            JSON.encodeToStream(jsonExportData, it)
+            JSON.encodeToStream(providerNode, it)
         }
     }
-    // Traversal Helper Functions:
 
-    private fun TagRecord.getUdtDefinition(): TagRecord {
-        require(config.tagType == "UdtInstance" || config.tagType == "UdtType") {
+    // Traversal Helper Functions:
+    private fun Node.getParentType(): Node {
+        require((isUdtDefinition() || isUdtInstance()) && config.typeId != null) {
             "Not a top level UDT Instance or type! $this"
         }
-        if (config.tagType == "UdtType") return this
-        return udtDefinitions[config.typeId]
+        return udtDefinitions[config.typeId.lowercase()]
             ?: throw IllegalArgumentException("Missing UDT Definition or Type ID. Current typeId: ${this.config.typeId}")
     }
 
-    private fun TagRecord.getDirectChildren(): List<TagRecord> {
-        return tagRecordData.filter { tagRecord -> tagRecord.folderId == id }
+    private fun Node.getFullPath(): String {
+        val lowercaseName = config.name!!.lowercase()
+        return if (folderId == "_types_") {
+            lowercaseName
+        } else {
+            "${nodeGroups[folderId!!]!!.parentNode.getFullPath()}/$lowercaseName"
+        }
     }
 
-    private fun TagRecord.getOriginalDefinition(): TagRecord {
-        fun TagRecord.getOriginalDefinitionRecursive(idParts: List<String>): TagRecord {
-            if (idParts.size == 1) {
-                val finalId = "$id.${idParts.first()}"
-                return tagRecordData.find { record -> record.id == finalId }!!
-            }
-            val topLevelInstanceId = "$id.${idParts.first()}"
-            val topLeveInstanceRecord = tagRecordData.find { record -> record.id == topLevelInstanceId }!!
-            val topLevelDefinition = topLeveInstanceRecord.getUdtDefinition()
+    private fun NodeGroup.resolveNestedChildInstances() {
+        require(parentNode.isUdtDefinition())
 
-            return topLevelDefinition.getOriginalDefinitionRecursive(idParts.subList(1, idParts.size))
+        val childInstances = childNodes.filter { it.isUdtInstance() }
+
+        for (childInstance in childInstances) {
+            val childDefinition = childInstance.getParentType()
+            // TODO: Handle missing definitions
+            val childDefinitionGroup =
+                nodeGroups[childDefinition.id] ?: throw IllegalStateException("This should never happen")
+
+            // nodeGroups being ordered by rank will make this check not happen very often
+            if (!childDefinitionGroup.isResolved) childDefinitionGroup.resolveInheritance()
+
+            copyChildrenFrom(childDefinitionGroup, instanceId = childInstance.id)
+        }
+    }
+
+    private fun NodeGroup.resolveInheritance() {
+        if (parentNode.isUdtDefinition()) resolveNestedChildInstances()
+
+        if (parentNode.config.typeId.isNullOrEmpty()) {
+            isResolved = true
+            return
         }
 
-        if (!config.tagType.isNullOrEmpty() && !config.name.isNullOrEmpty()) return this
-        val originalIdParts = id.split(".")
+        val inheritedParentNode = parentNode.getParentType()
+        // TODO: Handle missing definitions
+        val inheritedNodeGroup =
+            nodeGroups[inheritedParentNode.id] ?: throw IllegalStateException("This should never happen")
 
-        val topLevelInstanceId = originalIdParts.first()
-        val topLeveInstanceRecord = tagRecordData.find { record -> record.id == topLevelInstanceId }!!
-        val topLevelDefinition = topLeveInstanceRecord.getUdtDefinition()
+        if (!inheritedNodeGroup.isResolved) inheritedNodeGroup.resolveInheritance()
 
-        return topLevelDefinition.getOriginalDefinitionRecursive(originalIdParts.subList(1, originalIdParts.size))
+        copyChildrenFrom(inheritedNodeGroup)
+
+        isResolved = true
     }
 
     companion object {
         internal val JSON = Json {
-            ignoreUnknownKeys = false
-            explicitNulls = false
             prettyPrint = true
+            prettyPrintIndent = "  "
         }
-    }
-}
 
-data class TagRecord(
-    val id: String,
-    val providerId: Int,
-    val folderId: String?,
-    val config: TagConfig,
-    val rank: Int,
-    val name: String?,
-) {
-    @Suppress("unused")
-    val configAsJson: JsonObject
-        get() = TagConfigView.JSON.encodeToJsonElement(TagConfigSerializer, config).jsonObject
+        private fun Node.isUdtDefinition(): Boolean = config.tagType == "UdtType"
 
-    companion object {
-        fun extractUdtDefinitions(tagRecordData: List<TagRecord>): Map<String, TagRecord> = buildMap {
-            tagRecordData.forEach { record ->
-                if (record.config.tagType == "UdtType") {
-                    var fullPath = record.config.name!!
-                    var currentRecord = record
-                    while (currentRecord.folderId != "_types_") {
-                        currentRecord = tagRecordData.find { folderRecord ->
-                            folderRecord.id == currentRecord.folderId
-                        }!!
-                        fullPath = "${currentRecord.name}/$fullPath"
-                    }
-                    this[fullPath] = record
+        private fun Node.isUdtInstance(): Boolean = config.tagType == "UdtInstance"
+
+        val NodeGroup.parentNode: Node
+            get() = first()
+
+        val NodeGroup.childNodes: MutableList<Node>
+            get() = subList(1, size)
+
+        var NodeGroup.isResolved: Boolean
+            get() = first().resolved
+            set(value) {
+                first().resolved = value
+            }
+
+        private fun NodeGroup.resolveHierarchy() {
+            if (size == 1) return
+
+            for (i in 1..<size) {
+                val childNode = get(i)
+                find { node -> node.id == childNode.folderId }?.config?.tags?.add(childNode)
+            }
+        }
+
+        private fun NodeGroup.copyChildrenFrom(otherGroup: NodeGroup, instanceId: String = this.parentNode.id) {
+            otherGroup.childNodes.forEach { childNode ->
+                val newId = childNode.id.replace(otherGroup.parentNode.id, instanceId)
+                val newFolderId = childNode.folderId!!.replace(otherGroup.parentNode.id, instanceId)
+                val overrideNode = find { it.id == newId }
+
+                if (overrideNode == null) {
+                    add(
+                        childNode.copy(
+                            id = newId,
+                            folderId = newFolderId,
+                            config = TagConfig(
+                                name = childNode.config.name,
+                                tagType = childNode.config.tagType,
+                            ),
+                        ),
+                    )
+                } else {
+                    remove(overrideNode)
+                    add(
+                        overrideNode.copy(
+                            config = overrideNode.config.copy(
+                                name = childNode.config.name,
+                                tagType = childNode.config.tagType,
+                            ),
+                        ),
+                    )
                 }
             }
         }
