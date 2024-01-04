956848f28c0860696e9458926f92c0e0b2ad6536
Joshua Hansen <joshlha16@gmail.com>
2024-01-04 13:34:58 -0800

    Improve Tree UX. Fix Provider Stats bug

--- src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/TagConfigView.kt
+++ src/main/kotlin/io/github/inductiveautomation/kindling/idb/tagconfig/TagConfigView.kt
@@ -6,17 +6,20 @@ import io.github.inductiveautomation.kindling.idb.tagconfig.LazyTreeNode.Compani
 import io.github.inductiveautomation.kindling.idb.tagconfig.model.Node
 import io.github.inductiveautomation.kindling.idb.tagconfig.model.TagProviderRecord
 import io.github.inductiveautomation.kindling.utils.AbstractTreeNode
+import io.github.inductiveautomation.kindling.utils.Action
 import io.github.inductiveautomation.kindling.utils.EDT_SCOPE
 import io.github.inductiveautomation.kindling.utils.TabStrip
+import io.github.inductiveautomation.kindling.utils.attachPopupMenu
 import io.github.inductiveautomation.kindling.utils.configureCellRenderer
 import io.github.inductiveautomation.kindling.utils.tag
 import io.github.inductiveautomation.kindling.utils.treeCellRenderer
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import net.miginfocom.swing.MigLayout
+import java.awt.event.ItemEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.sql.Connection
@@ -25,11 +28,13 @@ import javax.swing.JComboBox
 import javax.swing.JFileChooser
 import javax.swing.JOptionPane
 import javax.swing.JPanel
+import javax.swing.JPopupMenu
 import javax.swing.JScrollPane
 import javax.swing.JSplitPane
 import javax.swing.JTree
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeModel
+import javax.swing.tree.TreeNode
 import javax.swing.tree.TreePath
 import kotlin.io.path.Path
 
@@ -66,23 +71,25 @@ class TagConfigView(connection: Connection) : ToolPanel() {
             selectedIndex = -1
 
             addItemListener { itemEvent ->
+                if (itemEvent.stateChange != ItemEvent.SELECTED) return@addItemListener
+
                 val selectedTagProvider = itemEvent.item as TagProviderRecord
 
-                CoroutineScope(Dispatchers.Default).launch {
-                    selectedTagProvider.initProviderNode()
-
-                    val lazyTreeNode = createLazyTreeStructure(selectedTagProvider)
-
-                    EDT_SCOPE.launch {
-                        tabs.setTitleAt(0, selectedTagProvider.name)
-                        providerTab.provider = selectedTagProvider
-                        tagProviderTree.model = DefaultTreeModel(lazyTreeNode)
+                EDT_SCOPE.launch {
+                    val lazyTreeNode = withContext(Dispatchers.Default) {
+                        selectedTagProvider.initProviderNode()
+                        createLazyTreeStructure(selectedTagProvider)
                     }
+
+                    tabs.setTitleAt(0, selectedTagProvider.name)
+                    providerTab.provider = selectedTagProvider
+                    tagProviderTree.model = DefaultTreeModel(lazyTreeNode)
                 }
             }
 
             // Dummy Tag Provider Record for preferred size
-            prototypeDisplayValue = (tagProviderData + TagProviderRecord(
+            prototypeDisplayValue = (
+                tagProviderData + TagProviderRecord(
                     dbConnection = connection,
                     description = "",
                     name = "Select a Tag Provider...",
@@ -103,19 +110,37 @@ class TagConfigView(connection: Connection) : ToolPanel() {
     private val tagProviderTree =
         JTree(DefaultMutableTreeNode("Select a Tag Provider to Browse")).apply {
             isRootVisible = false
+            showsRootHandles = true
+
             addMouseListener(
                 object : MouseAdapter() {
                     override fun mousePressed(e: MouseEvent?) {
                         if (e?.clickCount == 2) {
                             selectionPath?.let {
-                                if (it.toTagPath() !in tabs.indices.map(tabs::getToolTipTextAt)) {
-                                    tabs.addTab(NodeConfigPanel(it))
+                                if ((it.lastPathComponent as TreeNode).childCount == 0) {
+                                    if (it.toTagPath() !in tabs.indices.map(tabs::getToolTipTextAt)) {
+                                        tabs.addTab(NodeConfigPanel(it))
+                                    }
                                 }
                             }
                         }
                     }
                 },
             )
+
+            attachPopupMenu {
+                val pathAtPoint = getClosestPathForLocation(it.x, it.y)
+                selectionPath = pathAtPoint
+                JPopupMenu().apply {
+                    add(
+                        Action("Open Config") {
+                            if (pathAtPoint.toTagPath() !in tabs.indices.map(tabs::getToolTipTextAt)) {
+                                tabs.addTab(NodeConfigPanel(pathAtPoint))
+                            }
+                        },
+                    )
+                }
+            }
 
             cellRenderer = treeCellRenderer { _, value, _, expanded, _, _, _ ->
                 val actualValue = value as? LazyTreeNode
@@ -222,9 +247,7 @@ class LazyTreeNode(
                 tagType = node.config.tagType,
                 originalNode = node,
             ).apply {
-                for (childNode in node.config.tags) {
-                    children.add(fromNode(childNode))
-                }
+                children.addAll(node.config.tags.map(::fromNode))
             }
         }
     }
