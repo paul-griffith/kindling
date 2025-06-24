938c1b350fa46261f5e5dad6cdb0288c7de47734
Spencer Lommel <spencerlommel@gmail.com>
2025-06-24 13:08:09 -0500

    modified idb sql table timestamp column to align with idb log viewer

--- src/main/kotlin/io/github/paulgriffith/kindling/idb/generic/QueryResult.kt
+++ src/main/kotlin/io/github/paulgriffith/kindling/idb/generic/QueryResult.kt
@@ -1,5 +1,10 @@
 package io.github.paulgriffith.kindling.idb.generic
 
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import javax.swing.table.AbstractTableModel
 
 sealed interface QueryResult {
@@ -14,11 +19,33 @@ sealed interface QueryResult {
             require(columnNames.size == columnTypes.size)
         }
 
+        private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS")
+
         override fun getRowCount(): Int = data.size
         override fun getColumnCount(): Int = columnNames.size
         override fun getColumnName(columnIndex: Int): String = columnNames[columnIndex]
         override fun getColumnClass(columnIndex: Int): Class<*> = columnTypes[columnIndex]
-        override fun getValueAt(rowIndex: Int, columnIndex: Int): Any? = data[rowIndex][columnIndex]
+        override fun getValueAt(rowIndex: Int, columnIndex: Int): Any? {
+            val originalValue = data[rowIndex][columnIndex]
+            val columnType = columnTypes[columnIndex]
+            return when {
+                originalValue is Timestamp -> {
+                    dateTimeFormatter.format(
+                        originalValue.toInstant()
+                            .atZone(ZoneId.systemDefault())
+                            .toLocalDateTime()
+                    )
+                }
+                columnType == Timestamp::class.java && originalValue is Long -> {
+                    dateTimeFormatter.format(
+                        Instant.ofEpochMilli(originalValue)
+                            .atZone(ZoneId.systemDefault())
+                            .toLocalDateTime()
+                    )
+                }
+                else -> originalValue
+            }
+        }
     }
 
     class Error(
